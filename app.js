17feb9d61639c6235c430d5ecd868e510d4f5a88
ODAS <alexander.ayras@gmail.com>
2020-03-25 00:39:18 -0500

    Commit inicial

---
+++ app.js
@@ -0,0 +1,31 @@
+const argv = require('./config/yargs').argv;
+
+
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+
+//console.log(process.argv);
+
+// let argv2 = process.argv;
+
+// console.log('Limite ', argv.limite);
+
+// let parametro = argv[2];
+// let base = parametro.split('=')[1];
+
+let comando = argv._[0];
+
+switch (comando) {
+
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo Creado: ${archivo}`))
+            .catch(e => console.log(e));
+        break;
+
+    default:
+        console.log('Comando no reconocido');
+}
